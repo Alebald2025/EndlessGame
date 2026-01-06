01b20107560afa8ca798451d7b5de787d29bc68d
Alejandro Baldeon <a24josbalesp@inspedralbes.cat>
2026-01-06 21:26:30 +0100

    Movimiento izquierda y derecha entre carriles

---
+++ AGENTS.md
@@ -0,0 +1,4 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: EndlessGameGit
+- Unity version: Unity 2022.3.45f1
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/PlayerLaneMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class PlayerLaneController : MonoBehaviour {
+
+    public float laneDistance = 2.6f; // Distancia entre carriles
+    public float moveSpeed = 10f;
+    private int currentLane = 1;
+    private Vector3 targetPosition;
+
+    void Start() { 
+        UpdateTargetPosition(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow)) 
+            ChangeLane(-1); 
+        if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow)) 
+            ChangeLane(1);
+                           
+        transform.position = Vector3.MoveTowards( 
+            transform.position, 
+            targetPosition, 
+            moveSpeed * Time.deltaTime 
+        ); 
+    }
+    
+
+    void ChangeLane(int direction) { 
+        currentLane = Mathf.Clamp(currentLane + direction, 0, 2); 
+        UpdateTargetPosition(); 
+    }
+
+    void UpdateTargetPosition() { 
+        targetPosition = new Vector3((currentLane - 1) * laneDistance, transform.position.y, transform.position.z); 
+    }
+}
