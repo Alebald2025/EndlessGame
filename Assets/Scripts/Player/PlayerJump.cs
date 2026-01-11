b022c3723474b9fe352d960e458a64d46008dcde
Alejandro Baldeon <a24josbalesp@inspedralbes.cat>
2026-01-11 19:30:07 +0100

    no message

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -7,21 +7,31 @@ public class PlayerJump : MonoBehaviour
     public float jumpForce = 7f; 
     public bool isGrounded = true; 
     private Rigidbody rb; 
+    private Animator animator;
+
     void Start() 
     { 
         rb = GetComponent<Rigidbody>(); 
+        animator = GetComponent<Animator>();
     }
     void Update() 
     { 
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) 
-        { 
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
-            isGrounded = false; 
+        {
+            DoJump();
         } 
     }
     private void OnCollisionEnter(Collision collision) 
     { 
-        if (collision.collider.CompareTag("Ground")) isGrounded = true; 
+        if (collision.collider.CompareTag("Ground")) 
+            isGrounded = true; 
+    }
+
+    void DoJump()
+    {
+        animator.SetTrigger("Jump");
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded =  false;
     }
 
     public void TryJump()
